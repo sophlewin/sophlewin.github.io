4f4e341b803f35e2d1cc0b917585e029ff48fa41
x <sophlewin@outlook.com>
2024-05-20 16:36:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let bounceText = document.getElementById('bounce-text');
+    bounceText.classList.add('bounce');
+    
     let links = document.querySelectorAll('a');
+    
     links.forEach(link => {
         link.addEventListener('mouseover', function () {
             this.style.fontSize = '1.2em';
